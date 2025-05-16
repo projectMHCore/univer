813a60a942cf66e6a895c4f348d49770375cf581
Mykhail <project.mhcore@gmail.com>
2025-05-16 10:38:33 +0300

    .

--- laba_8/index.js
+++ laba_8/index.js
@@ -1,24 +1,16 @@
-var express = require("express");
-var app = express();
-
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
   socket.on('send message', function(msg){
-    console.log('message: ' + msg);
     io.emit('receive message', msg);
   });
 });
